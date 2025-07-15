6f8cbace272d17d95a9b995a0148dd33c3bf302e
Rustam Vurdihanov <rustamvurdihanov@gmail.com>
2025-07-15 20:33:54 +0700

    fixed url api/company_details/

--- project_run/urls.py
+++ project_run/urls.py
@@ -23,5 +23,5 @@ from app_run.views import company_details
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/company_details', company_details),
+    path('api/company_details/', company_details),
 ]
