085bdfa9aec5ef64bdbb8039f417127352055025
Rustam Vurdihanov <rustamvurdihanov@gmail.com>
2025-07-16 21:17:22 +0700

    added api/run (create serialized.py, model Run, view RunViewSet and router)

--- app_run/views.py
+++ app_run/views.py
@@ -1,6 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
+
 from django.conf import settings
+
+from .serializers import RunSerializer
+from .models import Run
 
 @api_view(['GET'])
 def company_details(request):
@@ -8,3 +13,8 @@ def company_details(request):
                'slogan': settings.SLOGAN,
                'contacts': settings.CONTACTS}
     return Response(details)
+
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
