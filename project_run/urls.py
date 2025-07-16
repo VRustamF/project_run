0cc0a174abd6c718addb14175765c86dbbe741f3
Rustam Vurdihanov <rustamvurdihanov@gmail.com>
2025-07-16 21:17:22 +0700

    added api/run

--- project_run/urls.py
+++ project_run/urls.py
@@ -14,14 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
+from rest_framework.routers import DefaultRouter
 
-from app_run.views import company_details
+from django.contrib import admin
+from django.urls import path, include
+
+from app_run.views import company_details, RunViewSet
+
+router = DefaultRouter()
+router.register('api/runs', viewset=RunViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/company_details/', company_details),
+    path('', include(router.urls))
 ]
