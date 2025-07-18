a0ff968d92ebac8f0ac8dd1fb85a190c3575f79b
Rustam Vurdihanov <rustamvurdihanov@gmail.com>
2025-07-18 17:57:52 +0700

    added athlete info to api/runs/

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -3,7 +3,15 @@ from rest_framework import serializers
 from .models import Run, User
 
 
+class UserSerializerForRun(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name']
+
+
 class RunSerializer(serializers.ModelSerializer):
+    athlete_data = UserSerializerForRun(read_only=True, source='athlete')
+
     class Meta:
         model = Run
         fields = '__all__'
